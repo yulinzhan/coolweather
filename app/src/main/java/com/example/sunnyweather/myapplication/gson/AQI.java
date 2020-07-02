3fe2164b57f727f976ec95c981c70d344ec98bb9
zyl1618223@163.com <zyl1618223@163.com>
2020-07-02 12:43:17 +0800

    最新提交

---
+++ app/src/main/java/com/example/sunnyweather/myapplication/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.sunnyweather.myapplication.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
